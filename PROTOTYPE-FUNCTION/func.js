cfff14e6f58aae3c307af3b5f725f8690a45e3cb
wasif_manzoor <wasifahmad679@gmail.com>
2025-09-20 03:00:39 +0530

    function prototype

---
+++ PROTOTYPE-FUNCTION/func.js
@@ -0,0 +1,15 @@
+Object.prototype.chai = function() {
+    console.log("Chai aur Code");
+};
+
+const tea = {
+    name: "Ice tea and lemon tea ",
+    typeo: "cool",
+};
+
+tea.chai(); // Yeh function ko call krta hai seedey 
+
+
+const myTeas = ["lemon", "orange"];
+
+myTeas.chai() // Yeh b function ko call krta hai seedey
