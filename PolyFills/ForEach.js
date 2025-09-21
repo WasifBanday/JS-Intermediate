14c41a39aaf2a4307f0e1bf03b47e76902034267
wasif_manzoor <wasifahmad679@gmail.com>
2025-09-21 18:23:12 +0530

    Forech

---
+++ PolyFills/ForEach.js
@@ -0,0 +1,20 @@
+if (!Array.prototype.myForEach) {
+    Array.prototype.myforEach = function(userFn) {
+        const originalArr = this; //  "this" Current object ki taraf point krta hai...
+
+        for (let i = 0; i < originalArr.length; i++) {
+            userFn(originalArr[i], i);
+        }
+    };
+}
+
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+// Error : If the .forEach function does not exist on arr varible ..
+// -- : No return , function Input , value , index 
+// , calls my fn for every value .
+
+const ret = arr.forEach(function(valuee, indexx) {
+    console.log(`My For Each Value at Index ${indexx} is ${valuee}`);
+});
