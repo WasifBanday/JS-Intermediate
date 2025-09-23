fa0d3b5eeac25dd6db436c2160cc86f36967dd27
wasif_manzoor <wasifahmad679@gmail.com>
2025-09-23 11:26:31 +0530

    DOM

--- PolyFills/ForEach.js
+++ PolyFills/ForEach.js
@@ -14,6 +14,10 @@ const arr = [1, 2, 3, 4, 5, 6];
 // Error : If the .forEach function does not exist on arr varible ..
 // -- : No return , function Input , value , index 
 // , calls my fn for every value .
+// - forEach takes a callback function.
+// - That function runs once for each item in the array.
+
+
 
 const ret = arr.forEach(function(valuee, indexx) {
     console.log(`My For Each Value at Index ${indexx} is ${valuee}`);
--- PolyFills/basic.js
+++ PolyFills/basic.js
@@ -1,7 +1,7 @@
 // If some function is not available in any browser it creates that function for the user ?
 
 if (!Array.prototype.fill) {
-    // Fallback - Polyfill
+    // Fallback - Polyfill - Its like a backup function
     Array.prototype.fill = function() {
 
     }
